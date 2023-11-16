95e38c6b038018d74f2080f9f3ff8d2aedbcc970
guiPython <guipython2001@gmail.com>
2023-11-16 11:06:18 -0300

    update: using lib edge weight support

--- conectando-povoados-distantes/exercicio.c
+++ conectando-povoados-distantes/exercicio.c
@@ -11,32 +11,27 @@
 #include <stdbool.h>
 #include <math.h>
 
-typedef struct Pair {
-    int node;
-    double weight;
-} Pair;
-
 typedef struct Heap {
     int capacity;
     int size;
     int* node_position;
-    Pair** pairs;
+    Pair* pairs;
 } Heap;
 
 int min(int a, int b);
 int max(int a, int b);
 
 Heap* heap(int size);
-Pair* getMin(Heap*);
+Pair getMin(Heap*);
 void heapify(Heap*, int index);
-void swap(int index, int other, Pair** items);
-void update(Heap*, int node, double weight);
+void swap(int index, int other, Pair* items);
+void update(Heap*, Pair);
 bool isEmpty(Heap);
 double weightByIndex(Heap heap, int index);
 bool contains(Heap, int node);
 void destroy(Heap*);
 
-void prim(Graph g, double** weights);
+void prim(Graph g);
 
 int main() {
     int vertices, edges, u, v;
@@ -45,31 +40,22 @@ int main() {
     scanf("%d %d", &vertices, &edges);
     Graph g = graph(vertices);
 
-    double* weights[vertices];
-    for(int i = 0; i < vertices; i++) {
-        weights[i] = malloc(sizeof(double) * vertices);
-    }
-
     for(int i = 0; i < edges; i++) {
         scanf("%d %d %lf", &u, &v, &w);
-        graph_insert_edge(g, edge(u, v));
-        weights[u][v] = w;
-        weights[v][u] = w;
+        graph_insert_edge(g, edge(u, v, w));
     }
 
-    prim(g, weights);
+    prim(g);
     graph_destroy(g);
-    for(int i = 0; i < vertices; i++) free(weights[i]);
     return 0;
 }
 
-void prim(Graph g, double** weights) {
+void prim(Graph g) {
     double total = 0.0;
     int order = graph_order(g);
     Heap* h = heap(order);
     int pred[order];
     double weight[order];
-    
 
     for(int i = 0; i < order; i++) {
         pred[i] = -1;
@@ -80,18 +66,18 @@ void prim(Graph g, double** weights) {
     pred[0] = 0;
 
     while(!isEmpty(*h)) {
-        Pair* pair = getMin(h);
-        int node = pair->node;
+        Pair pair = getMin(h);
+        int node = pair.v;
         int degree = graph_vertex_degree(g, node);
-        int neighbors[degree];
-        graph_neighbors(g, node, neighbors);
+        Pair neighbors[degree];
+        graph_neighbors_with_weight(g, node, neighbors);
 
         for(int i = 0; i < degree; i++) {
-            int neighbor = neighbors[i];
-            if(contains(*h, neighbor) && weight[neighbor] > weights[node][neighbor]) {
-                pred[neighbor] = node;
-                weight[neighbor] = weights[node][neighbor];
-                update(h, neighbor, weight[neighbor]);
+            Pair neighbor = neighbors[i];
+            if(contains(*h, neighbor.v) && weight[neighbor.v] > neighbor.weight) {
+                pred[neighbor.v] = node;
+                weight[neighbor.v] = neighbor.weight;
+                update(h, neighbor);
             }
         }
     }
@@ -106,38 +92,29 @@ void prim(Graph g, double** weights) {
     destroy(h);
 }
 
-Pair* pair(int node, double weight) {
-    Pair* pair = malloc(sizeof(Pair));
-    pair->node = node;
-    pair->weight = weight;
-    return pair;
-}
-
 Heap* heap(int capacity) {
     Heap* heap = malloc(sizeof(Heap));
     heap->size = capacity;
     heap->capacity = capacity;
     heap->node_position = malloc(sizeof(int) * capacity);
-    heap->pairs = malloc(sizeof(Pair*) * capacity);
+    heap->pairs = malloc(sizeof(Pair) * capacity);
 
     for(int i = 0; i < capacity; i++) {
         heap->node_position[i] = i;
         heap->pairs[i] = pair(i, INFINITY); 
     }
 
-    heap->pairs[0]->weight = 0.0f;
+    heap->pairs[0].weight = 0.0f;
     return heap;
 }
 
-Pair* getMin(Heap* heap) {
-    if(isEmpty(*heap)) return NULL;
- 
-    Pair* first = heap->pairs[0];
-    Pair* last = heap->pairs[heap->size - 1];
+Pair getMin(Heap* heap) {
+    Pair first = heap->pairs[0];
+    Pair last = heap->pairs[heap->size - 1];
     heap->pairs[0] = last;
  
-    heap->node_position[first->node] = heap->size - 1;
-    heap->node_position[last->node] = 0;
+    heap->node_position[first.v] = heap->size - 1;
+    heap->node_position[last.v] = 0;
  
     heap->size--;
     heapify(heap, 0);
@@ -160,25 +137,25 @@ void heapify(Heap* heap, int idx) {
         smallest = right;
  
     if (smallest != idx) {
-        Pair* smallest_pair = heap->pairs[smallest];
-        Pair* idx_pair = heap->pairs[idx];
+        Pair smallest_pair = heap->pairs[smallest];
+        Pair idx_pair = heap->pairs[idx];
  
-        heap->node_position[smallest_pair->node] = idx;
-        heap->node_position[idx_pair->node] = smallest;
+        heap->node_position[smallest_pair.v] = idx;
+        heap->node_position[idx_pair.v] = smallest;
  
         swap(smallest,idx, heap->pairs);
         heapify(heap, smallest);
     }
 }
 
-void update(Heap* heap, int node, double new_weight) {
-    int position = heap->node_position[node];
+void update(Heap* heap, Pair pair) {
+    int position = heap->node_position[pair.v];
     int parent = (position - 1) / 2;
-    heap->pairs[position]->weight = new_weight;
+    heap->pairs[position] = pair;
 
-    while(parent >= 0 && weightByIndex(*heap, parent) > new_weight) {
-        heap->node_position[node] = parent;
-        heap->node_position[heap->pairs[parent]->node] = position;
+    while(parent >= 0 && weightByIndex(*heap, parent) > pair.weight) {
+        heap->node_position[pair.v] = parent;
+        heap->node_position[heap->pairs[parent].v] = position;
         swap(parent, position, heap->pairs);
         position = parent;
         parent = (parent - 1) / 2;
@@ -196,11 +173,11 @@ bool contains(Heap heap, int node) {
 }
 
 double weightByIndex(Heap heap, int index) {
-    return heap.pairs[index]->weight;
+    return heap.pairs[index].weight;
 }
 
-void swap(int index, int other, Pair** items) {
-    Pair* aux = items[index];
+void swap(int index, int other, Pair* items) {
+    Pair aux = items[index];
     items[index] = items[other];
     items[other] = aux;
 }
